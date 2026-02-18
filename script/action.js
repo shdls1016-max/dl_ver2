5b7d7e349b36d12d6a4caaeb46ee5c9fd56d8e6c
shdls1016 <shdls1016@gmail.com>
2026-02-18 17:09:19 +0900

    hamburgermain

--- script/action.js
+++ script/action.js
@@ -1,7 +1,37 @@
-/* $('nav').on('mouseenter',function(){
-    $('header').addClass('active');
+$('.hamburgerBox.mo').click(function(){
+    $('header').toggleClass('on');
+    $('.hamburger').toggleClass('on');
+    
+    if($('header').hasClass('on')){
+        setTimeout(()=>{
+            $('h1').addClass('hide');
+        },400);
+        $('body').addClass('no-scroll')
+    } else{
+        $('h1').removeClass('hide');
+        $('.gnb > li').removeClass('open');
+         $('body').removeClass('no-scroll')
+    }
 });
 
-$('nav').on('mouseleave', function(){
-    $('header').removeClass('active');
-}); */
+$('.gnb > li > a').on('click', function (e) {
+
+  // ✅ 햄버거(모바일 메뉴) 열린 상태가 아니면 그냥 링크 동작
+  if (!$('header').hasClass('on')) return;
+
+  e.preventDefault();
+
+  const $li = $(this).parent();
+
+  if ($li.find('.lnb').length===0){
+    window.location.href = $(this).attr('href');;
+    return;
+  }
+
+  if ($li.hasClass('open')) {
+    $li.removeClass('open');
+  } else {
+    $('.gnb > li').removeClass('open'); // 다른 메뉴 닫기
+    $li.addClass('open');
+  }
+});
